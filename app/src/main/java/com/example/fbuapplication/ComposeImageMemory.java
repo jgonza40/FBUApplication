8df35eb83ea107a98b7ef48bfd2e679f0c0c2e82
jgonza40 <jessica_gonzalez1@brown.edu>
2020-07-14 15:32:56 -0700

    adding layout for posts

---
+++ app/src/main/java/com/example/fbuapplication/ComposeImageMemory.java
@@ -0,0 +1,14 @@
+package com.example.fbuapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ComposeImageMemory extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_compose_image_memory);
+    }
+}

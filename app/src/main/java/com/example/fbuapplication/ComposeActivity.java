d434dfaccfe10ef0432462e1d28a1736ae5ee183
jgonza40 <jessica_gonzalez1@brown.edu>
2020-07-14 15:56:07 -0700

    adding flow from compose activity to specific compose activity

--- app/src/main/java/com/example/fbuapplication/ComposeActivity.java
+++ app/src/main/java/com/example/fbuapplication/ComposeActivity.java
@@ -2,13 +2,36 @@ package com.example.fbuapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ComposeActivity extends AppCompatActivity {
+
+    private Button btnImage;
+    private Button btnQuote;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_compose);
+        btnImage = findViewById(R.id.btnImage);
+        btnQuote = findViewById(R.id.btnQuote);
+
+        btnImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(ComposeActivity.this, ComposeImageMemory.class);
+                startActivity(i);
+            }
+        });
+        btnQuote.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(ComposeActivity.this, ComposeQuoteMemory.class);
+                startActivity(i);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/fbuapplication/fragments/MapFragment.java
+++ app/src/main/java/com/example/fbuapplication/fragments/MapFragment.java
@@ -1,18 +1,24 @@
 package com.example.fbuapplication.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import com.example.fbuapplication.ComposeActivity;
+import com.example.fbuapplication.ComposeImageMemory;
 import com.example.fbuapplication.R;
 
 
 public class MapFragment extends Fragment {
-
+    private Button btnCompose;
 
     public MapFragment() {
         // Required empty public constructor
@@ -24,5 +30,18 @@ public class MapFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_map, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        btnCompose = view.findViewById(R.id.btnCompose);
+        btnCompose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getActivity(), ComposeActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }

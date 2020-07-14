524ef59410583cec8457e9a97317f7762ef5646b
jgonza40 <jessica_gonzalez1@brown.edu>
2020-07-13 22:29:55 -0700

    added logout

--- app/src/main/java/com/example/fbuapplication/MainActivity.java
+++ app/src/main/java/com/example/fbuapplication/MainActivity.java
@@ -1,10 +1,15 @@
 package com.example.fbuapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,8 +26,31 @@ public class MainActivity extends AppCompatActivity {
         getSupportActionBar().setTitle(null);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        return super.onCreateOptionsMenu(menu);
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        getMenuInflater().inflate(R.menu.main_menu, menu);
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        if (item.getItemId() == R.id.settings_icon) {
+//            Toast.makeText(this, "clicked!", Toast.LENGTH_SHORT).show();
+//            // Composed icon has been tapped
+//            // Navigate to the compose activity
+//            Intent intent = new Intent(this, Settings.class);
+//            startActivity(intent);
+//
+//            return true;
+//        }
+//        return super.onOptionsItemSelected(item);
+//    }
+
+    public void goToSettings(View view){
+        Toast.makeText(this, "clicked!", Toast.LENGTH_SHORT).show();
+        // Composed icon has been tapped
+        // Navigate to the compose activity
+        Intent intent = new Intent(this, Settings.class);
+        startActivity(intent);
     }
 }

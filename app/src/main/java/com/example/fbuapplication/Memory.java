bb6609639fd5fcd3431c14a386eda60199ed327a
jgonza40 <jessica_gonzalez1@brown.edu>
2020-07-14 20:11:20 -0700

    created memory class

---
+++ app/src/main/java/com/example/fbuapplication/Memory.java
@@ -0,0 +1,66 @@
+package com.example.fbuapplication;
+
+import com.parse.ParseClassName;
+import com.parse.ParseFile;
+import com.parse.ParseObject;
+import com.parse.ParseUser;
+
+@ParseClassName("Memory")
+public class Memory extends ParseObject {
+
+    public static final String KEY_DESCRIPTION = "description";
+    public static final String KEY_IMAGE = "image";
+    public static final String KEY_USER = "user";
+    public static final String KEY_QUOTE = "quote";
+    public static final String KEY_MEMORY_TITLE = "memoryTitle";
+    public static final String KEY_CATEGORY = "category";
+    public static final String KEY_CREATED_AT = "createdAt";
+
+    public String getDescription(){
+        return getString(KEY_DESCRIPTION);
+    }
+
+    public void setDescription(String description){
+        put(KEY_DESCRIPTION, description);
+    }
+
+    public ParseFile getImage(){
+        return getParseFile(KEY_IMAGE);
+    }
+
+    public void setImage(ParseFile parseFile){
+        put(KEY_IMAGE, parseFile);
+    }
+
+    public ParseUser getUser(){
+        return getParseUser(KEY_USER);
+    }
+
+    public void setUser(ParseUser user){
+        put(KEY_USER, user);
+    }
+
+    public String getQuote(){
+        return getString(KEY_QUOTE);
+    }
+
+    public void setQuote(String quote){
+        put(KEY_QUOTE, quote);
+    }
+
+    public String getMemoryTitle(){
+        return getString(KEY_MEMORY_TITLE);
+    }
+
+    public void setMemoryTitle(String memoryTitle){
+        put(KEY_MEMORY_TITLE, memoryTitle);
+    }
+
+    public String getCategory(){
+        return getString(KEY_CATEGORY);
+    }
+
+    public void setCategory(int category){
+        put(KEY_CATEGORY, category);
+    }
+}
--- app/src/main/java/com/example/fbuapplication/fragments/HomeFragment.java
+++ app/src/main/java/com/example/fbuapplication/fragments/HomeFragment.java
@@ -2,27 +2,66 @@ package com.example.fbuapplication.fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.fbuapplication.Memory;
 import com.example.fbuapplication.R;
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+
+import java.util.List;
 
 
 public class HomeFragment extends Fragment {
 
+    public static final String TAG = "HomeFragment";
+    public static final int MAX_POSTS = 20;
 
     public HomeFragment() {
         // Required empty public constructor
     }
-
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        queryPosts();
+    }
+
+    protected void queryPosts() {
+        // Specify which class to query
+        ParseQuery<Memory> query = ParseQuery.getQuery(Memory.class);
+        query.include(Memory.KEY_USER);
+        query.setLimit(MAX_POSTS);
+        query.addDescendingOrder(Memory.KEY_CREATED_AT);
+        query.findInBackground(new FindCallback<Memory>() {
+            @Override
+            public void done(List<Memory> memories, ParseException e) {
+                if (e != null) {
+                    Log.e(TAG, "Issue with getting posts", e);
+                    return;
+                }
+                for (Memory memory : memories) {
+                    Log.i(TAG, "Post: " + memory.getDescription() + ", Username: " + memory.getUser().getUsername());
+                }
+//                adapter.clear();
+//                adapter.addAll(posts);
+//                swipeContainer.setRefreshing(false);
+//                adapter.notifyDataSetChanged();
+            }
+        });
     }
 }

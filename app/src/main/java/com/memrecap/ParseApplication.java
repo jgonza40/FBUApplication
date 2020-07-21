b3081768a6704a1a6b36445948a86fc14368e135
jgonza40 <jessica_gonzalez1@brown.edu>
2020-07-21 12:44:09 -0700

    improving code style in parse app

--- app/src/main/java/com/memrecap/ParseApplication.java
+++ app/src/main/java/com/memrecap/ParseApplication.java
@@ -17,12 +17,10 @@ public class ParseApplication extends Application {
         ParseObject.registerSubclass(Memory.class);
         ParseObject.registerSubclass(Marker.class);
 
-        // set applicationId, and server server based on the values in the Heroku settings.
-        // clientKey is not needed unless explicitly configured
-        // any network interceptors must be added with the Configuration Builder given this syntax
+        // Referring to Heroku App Setting Strings
         Parse.initialize(new Parse.Configuration.Builder(this)
-                .applicationId("mem-recap") // should correspond to APP_ID env variable
-                .clientKey("myMemRecapSecretString111122")  // set explicitly unless clientKey is explicitly configured on Parse server
+                .applicationId("mem-recap")
+                .clientKey("myMemRecapSecretString111122")
                 .server("https://mem-recap.herokuapp.com/parse/").build());
     }
 }

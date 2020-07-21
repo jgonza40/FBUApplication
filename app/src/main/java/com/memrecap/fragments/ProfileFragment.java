e1ff8ecd83e728561a8f547370b47ea0a4c1088a
jgonza40 <jessica_gonzalez1@brown.edu>
2020-07-21 14:46:09 -0700

    improving code style in prof frag

--- app/src/main/java/com/memrecap/fragments/ProfileFragment.java
+++ app/src/main/java/com/memrecap/fragments/ProfileFragment.java
@@ -10,17 +10,12 @@ import android.view.ViewGroup;
 
 import com.memrecap.R;
 
-
     public class ProfileFragment extends Fragment {
 
-    public ProfileFragment() {
-        // Required empty public constructor
-    }
+    public ProfileFragment() {}
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_profile, container, false);
     }
 }

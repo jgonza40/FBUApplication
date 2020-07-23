b7aba9b16d1c8179b4d87f1df6ca1aa4e45cf059
jgonza40 <jessica_gonzalez1@brown.edu>
2020-07-23 12:54:36 -0700

    layout setup for swiping through location recap

--- app/src/main/java/com/memrecap/activities/LocationRecapActivity.java
+++ app/src/main/java/com/memrecap/activities/LocationRecapActivity.java
@@ -3,14 +3,76 @@ package com.memrecap.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.Toast;
 
+import com.lorentzos.flingswipe.SwipeFlingAdapterView;
 import com.memrecap.R;
 
+import java.util.ArrayList;
+
 public class LocationRecapActivity extends AppCompatActivity {
+
+    private ArrayList<String> al;
+    private ArrayAdapter<String> arrayAdapter;
+    private int i;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_location_recap);
+
+
+        al = new ArrayList<>();
+        al.add("example 1");
+        al.add("example 2");
+        al.add("example 3");
+        al.add("example 4");
+
+        arrayAdapter = new ArrayAdapter<>(this, R.layout.item_location_recap, R.id.helloText, al );
+
+        SwipeFlingAdapterView flingContainer;
+        flingContainer = (SwipeFlingAdapterView) findViewById(R.id.frame);
+        flingContainer.setAdapter(arrayAdapter);
+        flingContainer.setFlingListener(new SwipeFlingAdapterView.onFlingListener() {
+            @Override
+            public void removeFirstObjectInAdapter() {
+                // this is the simplest way to delete an object from the Adapter (/AdapterView)
+                Log.d("LIST", "removed object!");
+                al.remove(0);
+                arrayAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onLeftCardExit(Object dataObject) {
+                Toast.makeText(LocationRecapActivity.this, "Left!", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onRightCardExit(Object o) {
+            }
+
+            @Override
+            public void onAdapterAboutToEmpty(int itemsInAdapter) {
+                // Appending more data once arraylist is empty
+                al.add("XML ".concat(String.valueOf(i)));
+                arrayAdapter.notifyDataSetChanged();
+                i++;
+            }
+
+            @Override
+            public void onScroll(float scrollProgressPercent) {
+            }
+        });
+
+
+        flingContainer.setOnItemClickListener(new SwipeFlingAdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClicked(int itemPosition, Object dataObject) {
+                Toast.makeText(LocationRecapActivity.this, "Clicked!", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
